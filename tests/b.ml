b5d4408e0f307d8ab27348dd0a43b929ec587fde
Martin Pépin <kerl@wkerl.me>
2022-10-07 20:53:33 +0200

    Checking that multi-line tests work

--- tests/b.ml
+++ tests/b.ml
@@ -1,3 +1,14 @@
+(** This is a multi line test
+
+    {@doctest [
+      # let open Testlib.B in
+        let x = 21 in
+        let one = 1 in
+        multiply x (add one one);;
+      - : int = 42
+    ]}
+*)
+
 let add x y = x + y
 
 let multiply x y = x * y
